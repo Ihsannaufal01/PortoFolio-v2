976d6237e01e0aff60f25ca05aa943788dfe9cab
Ihsannaufal01 <azhimanaufal@gmail.com>
2024-10-15 09:23:25 +0700

    Initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,12 +5,15 @@ const Contact = () => {
         <section id="contact" className="py-20 bg-gray-100">
             <div className="container mx-auto px-6 text-center">
                 <h2 className="text-4xl font-bold mb-2 text-gray-800">Contact me</h2>
-                <p className="text-xl mb-5 text-gray-600">
-                    jangan ragu untuk menghubungi saya!
-                </p>
-                <a href="mailto:azhimanaufal@gmail.com" className="bg-rose-700 hover:bg-rose-500 text-white font-bold py-2 px-3 rounded-lg shadow-md transition duration-300">
+                <p className="text-xl mb-5 text-gray-600"> jangan ragu untuk menghubungi saya! </p>
+                
+                <a href="mailto:azhimanaufal@gmail.com" className="bg-rose-700 hover:bg-rose-500 text-white font-bold py-2 px-3 mx-2 rounded-lg shadow-md transition duration-300">
                     Send Mail
                 </a>
+                <a href="https://wa.me/6287719949449" className="bg-rose-700 hover:bg-rose-500 text-white font-bold py-2 px-3 mx-2 rounded-lg shadow-md transition duration-300">
+                    WhatsApp
+                </a>
+                
             </div>
         </section>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,10 @@ const Footer = () => {
             <div className="container mx-auto text-center">
                 <p className="text-sm mb-4"> &copy; {new Date().getFullYear()} Ihsan Naufal. </p>
             </div>
+            <div class="space-x-1">
+        <a href="https://github.com/Ihsannaufal01" class="text-gray-500 hover:text-gray-900"><i class="fab fa-github"></i> GitHub •</a>
+        <a href="https://instagram.com/noparusensei" class="text-gray-500 hover:text-gray-900"><i class="fab fa-instagram"></i>Instagram</a>
+        </div>
         </footer>
     )
 }
