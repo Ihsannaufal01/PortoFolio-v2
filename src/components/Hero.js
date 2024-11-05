108f9e0d35c27bed270822f895d3c5664a1aee67
Ihsannaufal01 <azhimanaufal@gmail.com>
2024-11-05 08:02:07 +0700

    second

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,7 @@ import Cover from '../asset/Hero.png'
 
 const Hero = () => {
     return (
-        <section className="bg-cover bg-center h-screen text-white"
-        style={{ backgroundImage: `url(${Cover})` }}
-        >
+        <section className="bg-cover bg-center h-screen text-white" style={{ backgroundImage: `url(${Cover})` }}>
             <div className=" bg-black bg-opacity-70 h-full flex flex-col justify-center items-center text-center p-6">
                 <h2 className="text-5xl font-bold">Halo, saya Ihsan!!</h2>
                 <p className=" text-2xl mb-8 pt-5">Web Developer Student</p>
